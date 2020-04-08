70fba6d85bbaef4dba334bd26998cbed9bd7a2c6
shreeganes <shreevidhya.ganesan@byon8.com>
2020-04-08 12:53:25 +0200

    commit

--- test_driver/main_test.dart
+++ test_driver/main_test.dart
@@ -1,27 +1,7 @@
-
-// Imports the Flutter Driver API.
-import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test/test.dart';
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:my_expense_app/main.dart' as expenseApp;
 
 void main() {
-  group('Meals App', () {
-    FlutterDriver driver;
-
-    // Connect to the Flutter driver before running any tests.
-    setUpAll(() async {
-      driver = await FlutterDriver.connect();
-    });
-
-    // Close the connection to the driver after the tests have completed.
-    tearDownAll(() async {
-      if (driver != null) {
-        driver.close();
-      }
-    });
-
-    test("Home Screen", () async {
-         expect(await driver.getText(find.text('No Transactions added yet')), 'No Transactions added yet');
-    });
-
-  });
+  enableFlutterDriverExtension();
+  expenseApp.main();
 }
