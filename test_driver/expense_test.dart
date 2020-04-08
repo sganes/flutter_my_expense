06e42496a477cb83252b93d8a227a5f6a9bb159e
shreeganes <shreevidhya.ganesan@byon8.com>
2020-04-08 13:41:55 +0200

    commit

--- test_driver/expense_test.dart
+++ test_driver/expense_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Meals App', () {
+  group('Expense App', () {
     FlutterDriver driver;
 
     // Connect to the Flutter driver before running any tests.
